c16787a865787702f7a2236de35995cc918d5f2c
youkidkk <youkidkk80@gmail.com>
2018-08-14 18:58:02 +0900

    DB（PostgreSQL）、言語、タイムゾーンの設定

--- djangosample/settings.py
+++ djangosample/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangosample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_sample',
+        'USER': 'django_sample',
+        'PASSWORD': 'django_sample',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
