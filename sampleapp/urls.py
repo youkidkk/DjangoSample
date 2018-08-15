4b3fa42345c07e5bd8bb266b24d2c849c639acb6
youkidkk <youkidkk80@gmail.com>
2018-08-15 14:34:45 +0900

    クエリパラメータのサンプルを追加

--- sampleapp/urls.py
+++ sampleapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ～/httpresponse
     path('httpresponse', views.httpresponse, name='httpresponse'),
+    # ～/queryparam?msg=・・・
+    path('queryparam', views.queryparam, name='queryparam'),
 ]
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def httpresponse(request):
     return HttpResponse("Hello!!!")
+
+def queryparam(request):
+    msg = "nothing"
+    if "msg" in request.GET:
+        msg = "'{0}'".format(request.GET["msg"])
+    return HttpResponse("Message is {0}.".format(msg))
