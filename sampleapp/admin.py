63e88556c6902602db905164323fcbfe6ab9aea7
youkidkk <youkidkk80@gmail.com>
2018-09-06 21:52:07 +0900

    ForeignKeySampleModel を追加

--- sampleapp/admin.py
+++ sampleapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import SampleModel
+from .models import ForeignKeySampleModel
 
 admin.site.register(SampleModel)
+admin.site.register(ForeignKeySampleModel)
--- sampleapp/models.py
+++ sampleapp/models.py
@@ -11,3 +11,7 @@ class SampleModel(models.Model):
     def __str__(self):
         return "Number: {0}, Text: {1}, Date: {2}, Time: {3}, Check: {4}".format(
             self.number, self.text, self.date, self.time, self.check)
+
+
+class ForeignKeySampleModel(models.Model):
+    sampleModel = models.ForeignKey(SampleModel, on_delete=models.CASCADE)
